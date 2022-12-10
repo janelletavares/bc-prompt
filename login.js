c21c3e1ad7d961e86daac980837b22ca783b5e5b
janelletavares <janelle@meroxa.io>
2022-12-10 12:51:58 -0800

    incomplete

--- login.js
+++ login.js
@@ -2,20 +2,12 @@ function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-        console.log("0Success!");
-        sleep(2000); //@TODO remove me
-    //let adminUsername = process.env.ADMIN_USERNAME
-        console.log("1Success!");
-        sleep(2000); //@TODO remove me
-    //let adminPassword = process.env.ADMIN_PASSWORD
-        console.log("2Success!");
-        sleep(2000); //@TODO remove me
-    let success = username === "admin" && password === "admin";
+    let adminUsername = process.env.ADMIN_USERNAME
+    let adminPassword = process.env.ADMIN_PASSWORD
+    let success = username === adminUsername && password === adminPassword;
     if (success) {
         console.log("Success!");
-        sleep(2000); //@TODO remove me
         localStorage.setItem("authenticated", "letsgo");
-        sleep(2000); //@TODO remove me
         window.location.href="./admin.html"; // i don't know why this isn't working
     } else {
         window.alert("Authentication failed");
